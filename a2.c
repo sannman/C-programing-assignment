318cd629be1a174e66cefe5f5fc54ed3222af2f0
Sannman Prabhu <prabhusanman2@gmail.com>
2025-09-21 20:08:48 +0530

    first commit

---
+++ a2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    float b;
+    char c;
+
+    a = 1;
+    b = 1.2;
+    c = "1";
+    printf("a = %d , b = %f , c=%c \n", a,b,c);
+    
+    return 0;
+
+}
---
+++ a3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    float a;
+    
+    scanf("%f", &a );
+
+    printf("You Entered %f \n",a);
+
+}
