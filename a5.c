25f3d3ff9cc4dd73541aa9ac82ffedf91ad9b266
Sannman Prabhu <prabhusanman2@gmail.com>
2025-09-21 23:23:52 +0530

    Add type conversion examples (a5.c)

---
+++ a5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+    // char to int
+    char a = 'A';
+    int b = a;
+    printf("char to int :%d \n", b);
+    // int to char
+    int c = 65;
+    char d = c;
+    printf("int to char :%c \n", d);
+    // int to float 
+    int e = 10;
+    float f = e;
+    printf("int to float : %f \n" , f);
+
+    //float to int 
+    float g = 9.8;
+    int h = g;
+    printf("float to int ; %d \n", h);
+}
